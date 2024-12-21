d936458a59f2a2e2de13bb3111227c1c5f266252
sama oruj <semaorucova08@gmail.com>
2024-12-21 15:01:43 +0300

    sema-7

--- Assets/Scripts/Gmovement.cs
+++ Assets/Scripts/Gmovement.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//dfdf
 public class Gmovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
